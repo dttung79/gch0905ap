b8866d9141c8d3af4c1bd73c2f6ef83554a05a7b
dttung79 <dttung@gmail.com>
2022-03-22 12:16:12 +0700

    update product

--- ProductProgram/ProductManager.cs
+++ ProductProgram/ProductManager.cs
@@ -7,6 +7,11 @@ namespace ProductProgram
     {
         private List<Product> listProducts;
 
+        public ProductManager()
+        {
+            listProducts = new List<Product>();
+        }
+
         public void AddNewProduct()
         {
             // ask user to enter name & price
@@ -28,11 +33,18 @@ namespace ProductProgram
             System.Console.Write("Enter product name to search: ");
             string name = Console.ReadLine();
             Product p = FindProduct(name);
-            // enter new price
-            System.Console.Write("Enter new price: ");
-            int price = Convert.ToInt32(Console.ReadLine());
-            // change product price
-            p.Price = price;
+            if (p == null)
+            {
+                System.Console.WriteLine("Product not found!");
+            }
+            else 
+            {
+                // enter new price
+                System.Console.Write("Enter new price: ");
+                int price = Convert.ToInt32(Console.ReadLine());
+                // change product price
+                p.Price = price;
+            }
         }
 
         public void DeleteProduct()
@@ -51,6 +63,16 @@ namespace ProductProgram
                 if (listProducts[i].Name == name) return listProducts[i];
 
             return null;
+        }
+
+        public void ShowProducts()
+        {
+            // C1: use traditional for loop
+            // for (int i = 0; i < listProducts.Count; i++)
+            //     listProducts[i].ShowInfo();
+            // C2: use foreach loop
+            foreach (Product p in listProducts)
+                p.ShowInfo();
         }
     }
 }
--- ProductProgram/Program.cs
+++ ProductProgram/Program.cs
@@ -6,7 +6,31 @@ namespace ProductProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ProductManager manager = new ProductManager();
+            while (true)
+            {
+                // print menu 
+                System.Console.WriteLine("Product Manager");
+                System.Console.WriteLine("1. Add new product.");
+                System.Console.WriteLine("2. Edit product.");
+                System.Console.WriteLine("3. Delete product.");
+                System.Console.WriteLine("4. Show all products.");
+
+                // ask user to enter a choice
+                System.Console.Write("Enter your choice: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                // based on choice, do corresponding task
+                switch (choice)
+                {
+                    case 1: manager.AddNewProduct(); break;
+                    case 2: manager.EditProduct(); break;
+                    case 3: manager.DeleteProduct(); break;
+                    case 4: manager.ShowProducts(); break;
+                    case 0: break;
+                    default: System.Console.WriteLine("Invalid choice!"); break;
+                }
+            }
         }
     }
 }
